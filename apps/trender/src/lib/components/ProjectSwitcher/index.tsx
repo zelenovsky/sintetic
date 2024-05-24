eaaac1ec8d0ef3f2f6a08c0b044ae65a614946e3
Artem Zelenov <deeloyy@gmail.com>
2024-05-24 18:55:25 +0600

    fix payload loading

--- apps/trender/src/lib/components/ProjectSwitcher/index.tsx
+++ apps/trender/src/lib/components/ProjectSwitcher/index.tsx
@@ -1,9 +1,11 @@
-import Select from './Select'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 import SinteticLogo from '@/lib/svg-icons/sintetic-logo'
+import Select from './Select'
 import s from './project-switcher.module.css'
-import type { Payload } from 'payload'
 
-export default function ProjectSwitcher({ payload }: { payload: Payload }) {
+export default async function ProjectSwitcher() {
+  const payload = await getPayload({ config: configPromise })
   return (
     <div className={s.container}>
       <SinteticLogo className={s.sinteticLogo} />
