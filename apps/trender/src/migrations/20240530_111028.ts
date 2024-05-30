e9e2b88583033ec8335bacaad4797468900f9e1b
Artem Zelenov <deeloyy@gmail.com>
2024-05-30 17:12:37 +0600

    fix migrations

--- apps/trender/src/migrations/20240530_111028.ts
+++ apps/trender/src/migrations/20240530_111028.ts
@@ -4,13 +4,13 @@ export async function up({ payload }: MigrateUpArgs): Promise<void> {
 await payload.db.drizzle.execute(sql`
 
 DO $$ BEGIN
- CREATE TYPE "_locales" AS ENUM('en', 'ru');
+ CREATE TYPE "_locales" AS ENUM('en');
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- CREATE TYPE "enum_users_type" AS ENUM('creator', 'user');
+ CREATE TYPE "enum_users_role" AS ENUM('super_admin', 'admin', 'user', 'editor');
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -21,12 +21,17 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
+DO $$ BEGIN
+ CREATE TYPE "enum__articles_v_version_status" AS ENUM('draft', 'published');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"username" varchar,
 	"phone" varchar,
 	"name" varchar,
-	"type" "enum_users_type",
+	"role" "enum_users_role" NOT NULL,
 	"description" varchar,
 	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
 	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
@@ -50,17 +55,13 @@ CREATE TABLE IF NOT EXISTS "users_rels" (
 CREATE TABLE IF NOT EXISTS "articles" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"content_html" varchar,
-	"status" "enum_articles_status",
-	"published_date" timestamp(3) with time zone,
 	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
-	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"_status" "enum_articles_status"
 );
 
 CREATE TABLE IF NOT EXISTS "articles_locales" (
-	"metadata_title" varchar,
-	"metadata_description" varchar,
-	"metadata_keywords" varchar,
-	"title" varchar NOT NULL,
+	"title" varchar,
 	"subtitle" varchar,
 	"description" varchar,
 	"content" jsonb,
@@ -75,6 +76,40 @@ CREATE TABLE IF NOT EXISTS "articles_rels" (
 	"order" integer,
 	"parent_id" integer NOT NULL,
 	"path" varchar NOT NULL,
+	"media_id" integer,
+	"users_id" integer,
+	"verticals_id" integer,
+	"tags_id" integer
+);
+
+CREATE TABLE IF NOT EXISTS "_articles_v" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"version_content_html" varchar,
+	"version_updated_at" timestamp(3) with time zone,
+	"version_created_at" timestamp(3) with time zone,
+	"version__status" "enum__articles_v_version_status",
+	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"latest" boolean
+);
+
+CREATE TABLE IF NOT EXISTS "_articles_v_locales" (
+	"version_title" varchar,
+	"version_subtitle" varchar,
+	"version_description" varchar,
+	"version_content" jsonb,
+	"id" serial PRIMARY KEY NOT NULL,
+	"_locale" "_locales" NOT NULL,
+	"_parent_id" integer NOT NULL,
+	CONSTRAINT "_articles_v_locales_locale_parent_id_unique" UNIQUE("_locale","_parent_id")
+);
+
+CREATE TABLE IF NOT EXISTS "_articles_v_rels" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"order" integer,
+	"parent_id" integer NOT NULL,
+	"path" varchar NOT NULL,
+	"articles_id" integer,
 	"media_id" integer,
 	"users_id" integer,
 	"verticals_id" integer,
@@ -105,6 +140,8 @@ CREATE TABLE IF NOT EXISTS "media_locales" (
 CREATE TABLE IF NOT EXISTS "verticals" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"slug" varchar,
+	"theme_dark_css" varchar,
+	"theme_light_css" varchar,
 	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
 	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
 );
@@ -172,6 +209,13 @@ CREATE INDEX IF NOT EXISTS "articles_created_at_idx" ON "articles" ("created_at"
 CREATE INDEX IF NOT EXISTS "articles_rels_order_idx" ON "articles_rels" ("order");
 CREATE INDEX IF NOT EXISTS "articles_rels_parent_idx" ON "articles_rels" ("parent_id");
 CREATE INDEX IF NOT EXISTS "articles_rels_path_idx" ON "articles_rels" ("path");
+CREATE INDEX IF NOT EXISTS "_articles_v_version_version_created_at_idx" ON "_articles_v" ("version_created_at");
+CREATE INDEX IF NOT EXISTS "_articles_v_created_at_idx" ON "_articles_v" ("created_at");
+CREATE INDEX IF NOT EXISTS "_articles_v_updated_at_idx" ON "_articles_v" ("updated_at");
+CREATE INDEX IF NOT EXISTS "_articles_v_latest_idx" ON "_articles_v" ("latest");
+CREATE INDEX IF NOT EXISTS "_articles_v_rels_order_idx" ON "_articles_v_rels" ("order");
+CREATE INDEX IF NOT EXISTS "_articles_v_rels_parent_idx" ON "_articles_v_rels" ("parent_id");
+CREATE INDEX IF NOT EXISTS "_articles_v_rels_path_idx" ON "_articles_v_rels" ("path");
 CREATE INDEX IF NOT EXISTS "media_created_at_idx" ON "media" ("created_at");
 CREATE UNIQUE INDEX IF NOT EXISTS "media_filename_idx" ON "media" ("filename");
 CREATE INDEX IF NOT EXISTS "verticals_created_at_idx" ON "verticals" ("created_at");
@@ -186,91 +230,133 @@ CREATE INDEX IF NOT EXISTS "payload_preferences_rels_parent_idx" ON "payload_pre
 CREATE INDEX IF NOT EXISTS "payload_preferences_rels_path_idx" ON "payload_preferences_rels" ("path");
 CREATE INDEX IF NOT EXISTS "payload_migrations_created_at_idx" ON "payload_migrations" ("created_at");
 DO $$ BEGIN
- ALTER TABLE "users_rels" ADD CONSTRAINT "users_rels_parent_id_users_id_fk" FOREIGN KEY ("parent_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "users_rels" ADD CONSTRAINT "users_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "users_rels" ADD CONSTRAINT "users_rels_media_id_media_id_fk" FOREIGN KEY ("media_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "users_rels" ADD CONSTRAINT "users_rels_media_fk" FOREIGN KEY ("media_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "articles_locales" ADD CONSTRAINT "articles_locales__parent_id_articles_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "articles"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "articles_locales" ADD CONSTRAINT "articles_locales_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "articles"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_parent_id_articles_id_fk" FOREIGN KEY ("parent_id") REFERENCES "articles"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "articles"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_media_id_media_id_fk" FOREIGN KEY ("media_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_media_fk" FOREIGN KEY ("media_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_users_id_users_id_fk" FOREIGN KEY ("users_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_verticals_id_verticals_id_fk" FOREIGN KEY ("verticals_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_verticals_fk" FOREIGN KEY ("verticals_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_tags_id_tags_id_fk" FOREIGN KEY ("tags_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "articles_rels" ADD CONSTRAINT "articles_rels_tags_fk" FOREIGN KEY ("tags_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "media_locales" ADD CONSTRAINT "media_locales__parent_id_media_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_locales" ADD CONSTRAINT "_articles_v_locales_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "_articles_v"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "verticals_locales" ADD CONSTRAINT "verticals_locales__parent_id_verticals_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_rels" ADD CONSTRAINT "_articles_v_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "_articles_v"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "tags_locales" ADD CONSTRAINT "tags_locales__parent_id_tags_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_rels" ADD CONSTRAINT "_articles_v_rels_articles_fk" FOREIGN KEY ("articles_id") REFERENCES "articles"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "tags_rels" ADD CONSTRAINT "tags_rels_parent_id_tags_id_fk" FOREIGN KEY ("parent_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_rels" ADD CONSTRAINT "_articles_v_rels_media_fk" FOREIGN KEY ("media_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "tags_rels" ADD CONSTRAINT "tags_rels_verticals_id_verticals_id_fk" FOREIGN KEY ("verticals_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_rels" ADD CONSTRAINT "_articles_v_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_parent_id_payload_preferences_id_fk" FOREIGN KEY ("parent_id") REFERENCES "payload_preferences"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_rels" ADD CONSTRAINT "_articles_v_rels_verticals_fk" FOREIGN KEY ("verticals_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
 DO $$ BEGIN
- ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_users_id_users_id_fk" FOREIGN KEY ("users_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "_articles_v_rels" ADD CONSTRAINT "_articles_v_rels_tags_fk" FOREIGN KEY ("tags_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "media_locales" ADD CONSTRAINT "media_locales_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "media"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "verticals_locales" ADD CONSTRAINT "verticals_locales_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "tags_locales" ADD CONSTRAINT "tags_locales_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "tags_rels" ADD CONSTRAINT "tags_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "tags"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "tags_rels" ADD CONSTRAINT "tags_rels_verticals_fk" FOREIGN KEY ("verticals_id") REFERENCES "verticals"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "payload_preferences"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -286,6 +372,9 @@ DROP TABLE "users_rels";
 DROP TABLE "articles";
 DROP TABLE "articles_locales";
 DROP TABLE "articles_rels";
+DROP TABLE "_articles_v";
+DROP TABLE "_articles_v_locales";
+DROP TABLE "_articles_v_rels";
 DROP TABLE "media";
 DROP TABLE "media_locales";
 DROP TABLE "verticals";
